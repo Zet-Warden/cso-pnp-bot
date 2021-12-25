638e1a94f89699df1dbf1604f1d55fcf74f1f2f7
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-12-26 01:57:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,134 +28,33 @@ app.post('/messages/api', async (req, res) => {
 
     // res.json({ type: 'message', textFormat: 'xml', text: `${textInfo}` });
     res.json({
-        contentType: 'application/vnd.microsoft.card.adaptive',
-        content: {
-            $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
-            type: 'AdaptiveCard',
-            version: '1.0',
-            body: [
-                {
-                    type: 'Container',
-                    items: [
+        type: 'message',
+        attachments: [
+            {
+                contentType: 'application/vnd.microsoft.card.adaptive',
+                contentUrl: null,
+                content: {
+                    type: 'AdaptiveCard',
+                    version: '1.4',
+                    body: [
                         {
                             type: 'TextBlock',
-                            text: 'Publish Adaptive Card schema',
-                            weight: 'bolder',
-                            size: 'medium',
+                            text: 'Request sent by: ' + receivedMsg.from.name,
                         },
                         {
-                            type: 'ColumnSet',
-                            columns: [
-                                {
-                                    type: 'Column',
-                                    width: 'auto',
-                                    items: [
-                                        {
-                                            type: 'Image',
-                                            url: 'https://pbs.twimg.com/profile_images/3647943215/d7f12830b3c17a5a9e4afcc370e3a37e_400x400.jpeg',
-                                            size: 'small',
-                                            style: 'person',
-                                        },
-                                    ],
-                                },
-                                {
-                                    type: 'Column',
-                                    width: 'stretch',
-                                    items: [
-                                        {
-                                            type: 'TextBlock',
-                                            text: 'Matt Hidinger',
-                                            weight: 'bolder',
-                                            wrap: true,
-                                        },
-                                        {
-                                            type: 'TextBlock',
-                                            spacing: 'none',
-                                            text: 'Created {{DATE(2017-02-14T06:08:39Z, SHORT)}}',
-                                            isSubtle: true,
-                                            wrap: true,
-                                        },
-                                    ],
-                                },
-                            ],
+                            type: 'Image',
+                            url: 'https://c.s-microsoft.com/en-us/CMSImages/DesktopContent-04_UPDATED.png?version=43c80870-99dd-7fb1-48c0-59aced085ab6',
+                        },
+                        {
+                            type: 'TextBlock',
+                            text: 'Sample image for Adaptive Card.',
                         },
                     ],
                 },
-                {
-                    type: 'Container',
-                    items: [
-                        {
-                            type: 'TextBlock',
-                            text: 'Now that we have defined the main rules and features of the format, we need to produce a schema and publish it to GitHub. The schema will be the starting point of our reference documentation.',
-                            wrap: true,
-                        },
-                        {
-                            type: 'FactSet',
-                            facts: [
-                                {
-                                    title: 'Board:',
-                                    value: 'Adaptive Card',
-                                },
-                                {
-                                    title: 'List:',
-                                    value: 'Backlog',
-                                },
-                                {
-                                    title: 'Assigned to:',
-                                    value: 'Matt Hidinger',
-                                },
-                                {
-                                    title: 'Due date:',
-                                    value: 'Not set',
-                                },
-                            ],
-                        },
-                    ],
-                },
-            ],
-            actions: [
-                {
-                    type: 'Action.ShowCard',
-                    title: 'Set due date',
-                    card: {
-                        type: 'AdaptiveCard',
-                        body: [
-                            {
-                                type: 'Input.Date',
-                                id: 'dueDate',
-                            },
-                        ],
-                        actions: [
-                            {
-                                type: 'Action.Submit',
-                                title: 'OK',
-                            },
-                        ],
-                    },
-                },
-                {
-                    type: 'Action.ShowCard',
-                    title: 'Comment',
-                    card: {
-                        type: 'AdaptiveCard',
-                        body: [
-                            {
-                                type: 'Input.Text',
-                                id: 'comment',
-                                isMultiline: true,
-                                placeholder: 'Enter your comment',
-                            },
-                        ],
-                        actions: [
-                            {
-                                type: 'Action.Submit',
-                                title: 'OK',
-                            },
-                        ],
-                    },
-                },
-            ],
-        },
+                name: null,
+                thumbnailUrl: null,
+            },
+        ],
     });
 });
 
