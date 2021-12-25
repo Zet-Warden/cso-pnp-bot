b429fb76fced98d0797cf10f5c782c5a0b79b611
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-12-26 02:58:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post('/messages/api', async (req, res) => {
     // console.log(opaNumber);
     const rowInfo = (await getRowInfo(opaNumber)).info;
     const textInfo = Object.keys(rowInfo).reduce((prev, currProp) => {
-        return `${prev}\n\n${currProp}:\t\t\t${rowInfo[currProp]}`;
+        return `${prev}\n\n${currProp}:&nbsp;&nbsp;&nbsp;&nbsp;${rowInfo[currProp]}`;
     }, '');
 
     // let textInfo =
