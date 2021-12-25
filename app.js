ac489976858a54820b2f751f06a0e83c5f60c51f
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-12-25 23:20:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const crypto = require('crypto');
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/messages/api', (req, res) => {
     res.json({ msg: 'OKAY' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at localhost:${PORT}`);
 });
