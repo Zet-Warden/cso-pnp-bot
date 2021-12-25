72417e5cc285fe40c0c3de6d63715709525f6828
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-12-25 23:27:07 +0800

    added env config

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const crypto = require('crypto');
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/messages/api', (req, res) => {
+    console.log('message request made');
     res.json({ msg: 'OKAY' });
 });
 
