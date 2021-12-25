785d08ae5999c7dcab39f097e091f75238d60689
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-12-26 03:08:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.post('/messages/api', async (req, res) => {
     const [, , opaNumber] = text.trim().split(' ');
     // console.log(opaNumber);
     const rowInfo = (await getRowInfo(opaNumber)).info;
-    const textInfo = Object.keys(rowInfo).reduce((prev, currProp) => {
-        return `${prev}\n\n${currProp}:&#09;&#09;&#09;${rowInfo[currProp]}`;
-    }, '');
+    // const textInfo = Object.keys(rowInfo).reduce((prev, currProp) => {
+    //     return `${prev}\n\n${currProp}:&#09;&#09;&#09;${rowInfo[currProp]}`;
+    // }, '');
 
     // let textInfo =
     //     '<table style=”padding:250px;width:500px;border:100px white;”> <tr style=”background-color:#c6c6c6"> <th>Number</th><th>Short Description</th></tr>';
@@ -28,12 +28,11 @@ app.post('/messages/api', async (req, res) => {
 
     // res.json({ type: 'message', textFormat: 'xml', text: `${textInfo}` });
 
-    /* const columnOne = {
+    const columnOne = {
         type: 'Column',
         items: Object.keys(rowInfo).map((key) => {
             return {
                 type: 'TextBlock',
-                wrap: true,
                 width: 'auto',
                 minHeight: '180px',
                 separator: true,
@@ -47,7 +46,6 @@ app.post('/messages/api', async (req, res) => {
         items: Object.keys(rowInfo).map((key) => {
             return {
                 type: 'TextBlock',
-                wrap: true,
                 width: '350px',
                 minHeight: '180px',
                 separator: true,
@@ -56,11 +54,72 @@ app.post('/messages/api', async (req, res) => {
         }),
     };
 
-    const columns = [columnOne, columnTwo]; */
+    const columns = [columnOne, columnTwo];
 
     res.json({
         type: 'message',
-        text: textInfo,
+        attachments: [
+            {
+                contentType: 'application/vnd.microsoft.card.adaptive',
+                contentUrl: null,
+                content: {
+                    type: 'AdaptiveCard',
+                    version: '1.5',
+                    body: [
+                        {
+                            type: 'Table',
+                            firstRowAsHeaders: true,
+                            columns: [
+                                {
+                                    width: 1,
+                                },
+                                {
+                                    width: 1,
+                                },
+                            ],
+                            rows: [
+                                {
+                                    type: 'TableRow',
+                                    cells: [
+                                        {
+                                            type: 'TextBlock',
+                                            text: 'Name',
+                                            wrap: true,
+                                            weigth: 'Bolder',
+                                        },
+                                        {
+                                            type: 'TextBlock',
+                                            text: 'Name',
+                                            wrap: true,
+                                            weigth: 'Bolder',
+                                        },
+                                    ],
+                                },
+                                {
+                                    type: 'TableRow',
+                                    cells: [
+                                        {
+                                            type: 'TextBlock',
+                                            text: 'Name',
+                                            wrap: true,
+                                            weigth: 'Bolder',
+                                        },
+                                        {
+                                            type: 'TextBlock',
+                                            text: 'Name',
+                                            wrap: true,
+                                            weigth: 'Bolder',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+                name: null,
+                thumbnailUrl: null,
+            },
+        ],
     });
 });
 
